3c02c40b2b87f718715c30dd65cb413a8d1a23b6
Isaac Hamm <isaachamm3@gmail.com>
2024-12-28 21:37:56 -0700

    feat(CreateSpreadsheet) Added context-checking for spreadsheet titles.
    I.e., prevents duplicates by ensuring that the title given is a unique one.

--- main.go
+++ main.go
@@ -11,11 +11,20 @@ import (
 
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
+
+	// Sheets API
 	"google.golang.org/api/sheets/v4"
+
+	// Drive API
+	"google.golang.org/api/drive/v3"
 )
 
 var sheetsService *sheets.Service
+var driveService *drive.Service
+
 const DEFAULT_FILE_PERMISSIONS = 0644
+
+var SCOPES []string = []string{"https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"}
 
 func main() {
 
@@ -26,7 +35,7 @@ func main() {
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
+	config, err := google.ConfigFromJSON(b, SCOPES...)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
@@ -37,8 +46,14 @@ func main() {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
 
+	driveService, err = drive.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Drive client: %v", err)
+	}
+
 	http.HandleFunc("/createSpreadsheet", createSpreadsheet)
 
+	fmt.Println("Starting server . . .")
 	err = http.ListenAndServe("127.0.0.1:3333", nil)
 
 	if errors.Is(err, http.ErrServerClosed) {
@@ -49,14 +64,14 @@ func main() {
 	}
 }
 
-type SpreadsheetCreation struct {
+type SpreadsheetCreationHttpRequest struct {
 	Title string
 }
 
 func createSpreadsheet(w http.ResponseWriter, r *http.Request) {
 
 	decoder := json.NewDecoder(r.Body)
-	var requestBody SpreadsheetCreation
+	var requestBody SpreadsheetCreationHttpRequest
 	err := decoder.Decode(&requestBody)
 	if err != nil {
 		panic(err)
@@ -64,7 +79,30 @@ func createSpreadsheet(w http.ResponseWriter, r *http.Request) {
 
 	var newTitle string = requestBody.Title
 
-	// newTitle := ""
+	fileList, err := driveService.Files.List().Do()
+	if err != nil {
+		log.Fatalf("Unable to perform query for drive files. Error: %v", err)
+	}
+
+	if len(fileList.Files) == 0 {
+
+		fmt.Println("No files found.")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Could not find any files in the drive"))
+		return
+
+	} else {
+		for _, i := range fileList.Files {
+			if i.Name == newTitle {
+				fmt.Printf("%s (%s)\n", i.Name, i.Id)
+				fmt.Println("Request received with sheet title already exists (Status 409)")
+				w.WriteHeader(http.StatusConflict)
+				w.Write([]byte("Sheet title already exists, please choose another"))
+				return
+			}
+		}
+	}
+
 	newSpreadsheet, err := sheetsService.Spreadsheets.Create(&sheets.Spreadsheet{Properties: &sheets.SpreadsheetProperties{Title: newTitle}}).Do()
 	if err != nil {
 		log.Fatalf("Unable to create new sheet with title: %s. Error: %v", newTitle, err)
@@ -72,9 +110,9 @@ func createSpreadsheet(w http.ResponseWriter, r *http.Request) {
 
 	if newSpreadsheet.Properties.Title != "" && newSpreadsheet.SpreadsheetId != "" {
 
+		// Add new Spreadsheet title/ID pair to data.json file
 		spreadsheetJsonFilePath := "data/spreadsheetIDs.json"
 		spreadsheetJsonFile, err := os.ReadFile(spreadsheetJsonFilePath)
-
 		if err != nil {
 			log.Fatalf("Unable to read Data Spreadsheet ID file: %v", err)
 		}
@@ -86,7 +124,6 @@ func createSpreadsheet(w http.ResponseWriter, r *http.Request) {
 		}
 
 		spreadsheetIdObject[newSpreadsheet.Properties.Title] = newSpreadsheet.SpreadsheetId
-
 		dataBytes, err := json.Marshal((spreadsheetIdObject))
 		if err != nil {
 			log.Fatalf("Unable to encode Data Spreadsheet ID JSON: %v", err)
@@ -102,7 +139,9 @@ func createSpreadsheet(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatalf("Error turning response body to JSON bytes. Error: %v", err)
 		}
+		w.WriteHeader(http.StatusCreated)
 		w.Write(responseBodyBytes)
+		return
 	}
 
 }
